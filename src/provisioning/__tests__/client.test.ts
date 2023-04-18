3e647b3657f8083ac31a9068281405974b0f2763
dennisja <dennisjjagwe@gmail.com>
2023-04-18 18:00:55 +0200

    chore(tests): add tests for creating the provisioning client

---
+++ src/provisioning/__tests__/client.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+
+import { createProvisioningClient } from '../..';
+
+describe('createProvisioningClient', () => {
+  it('should create a client with correct baseURL and default headers', () => {
+    const mockSubscriptionKey = 'mock key';
+    const client = createProvisioningClient({
+      subscriptionKey: mockSubscriptionKey,
+    });
+    expect(client.defaults.baseURL).toBe(
+      'https:/sandbox.momodeveloper.mtn.com/v1_0/apiuser'
+    );
+    expect(client.defaults.headers).toMatchObject({
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': mockSubscriptionKey,
+    });
+  });
+});
